d100e0b05c27ed1d2babe7600567681f84933d65
Mehmet Gultas <mehmetgultas@gmail.com>
2016-06-28 11:26:36 +0300

    Contains examples from the package that are use in the package.

---
+++ ornekler.R
@@ -0,0 +1,4 @@
+crosshairs(FergusonBrannick2012$pub_z, FergusonBrannick2012$dis_z,
+           FergusonBrannick2012$pub_z_se, FergusonBrannick2012$dis_z_se,
+           mdrtr = FergusonBrannick2012$mod, annotate = TRUE,
+           main_lab = 'Moderator Legend and Annotation')
